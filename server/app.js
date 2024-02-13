25dd974141084c66a26578b11441db9e33d0e171
seonyeonghun <sprax@naver.com>
2024-02-13 16:28:34 +0900

    HTTP_TEST | server 업데이트

--- server/app.js
+++ server/app.js
@@ -1,8 +1,9 @@
-import path from "path";
-import { fileURLToPath} from "url";
-import morgan from "morgan";
+import path from 'path';
+import { fileURLToPath } from 'url';
+import morgan from 'morgan';
 import express from 'express';
 import cors from 'cors';
+import db from './db.json' assert { type: 'json' };
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const app = express();
@@ -12,49 +13,53 @@ app.use(morgan('dev'));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use("/", express.static(__dirname+'/public'));
-// mock data
-const users = [
-  {
-    userNo: 1,
-    userId: 'test',
-    userPw: 'test1004',
-    userEmail: 'test@example.com',
-    userPhone: '010-1111-2222',
-  },
-  {
-    userNo: 2,
-    userId: 'hello',
-    userPw: 'hello1004',
-    userEmail: 'hello@example.com',
-    userPhone: '010-2222-3333',
-  },
-  {
-    userNo: 3,
-    userId: 'seon',
-    userPw: 'seon1004',
-    userEmail: 'seon@example.com',
-    userPhone: '010-3333-4444',
-  },
-];
-app.get("/", (req, res) => { // req: 요청정보를 담은 객체, res : 응답정보를 담은 객체
-  res.sendFile(path.join(__dirname, "/public/result.html"));
+app.use('/', express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  // req: 요청정보를 담은 객체, res : 응답정보를 담은 객체
+  res.sendFile(__dirname + '/public/result.html');
 });
-app.post('/', (req, res) => {
-  const { id, pw } = req.body;
-  for (const user of users) {
-    if (user.userId === id && user.userPw === pw) {
-      return res.json({
-        statusCode: 200,
-        statusMessage: 'success',
-      })
-    } else {
-      return res.json({
-        statusCode: 200,
-        statusMessage: 'fail'
-      })
+app.get('/srch', (req, res) => {
+  const obj = req.query;
+  const entries = Object.entries(obj);
+  let keywords = [];
+  let results = [];
+  for (const [key, value] of entries) {
+    console.log(`key : ${key}, value: ${value}`);
+    keywords = value.split(' ');
+  }
+
+  for (const food of db.foods) {
+    let isMatch = true;
+    for (const keyword of keywords) {
+      if (!food.region.includes(keyword) && !food.title.includes(keyword)) {
+        isMatch = false;
+        break;
+      }
+    }
+    // 일치하는 항목을 결과에 추가
+    if (isMatch) {
+      console.log('================ matched ==============');
+      results.push(food);
     }
   }
+  console.log(results);
+});
+
+app.post('/add', (req, res) => {
+  const { id, pw } = req.body;
+  const foundUser = users.filter((user) => {
+    if (id === user.userId && pw === user.userPw) {
+      res.json({
+        message: 'success',
+        data: foundUser,
+      });
+    } else {
+      res.json({
+        message: 'fail',
+      });
+    }
+  });
 });
 
 app.listen(port, () => {
